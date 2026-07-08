715f37bdced57d7e7d61735a02949b0534434799
Lucas Cristin <lcristin@frba.utn.edu.ar>
2026-07-07 23:49:16 -0300

    fix

--- src/main/java/ar/edu/utn/edu/lucas_cristin/sistema_climalert/repositories/memory/impl/AlertaRepositorioImplMemoria.java
+++ src/main/java/ar/edu/utn/edu/lucas_cristin/sistema_climalert/repositories/memory/impl/AlertaRepositorioImplMemoria.java
@@ -1,13 +1,12 @@
 package ar.edu.utn.edu.lucas_cristin.sistema_climalert.repositories.memory.impl;
 
 import ar.edu.utn.edu.lucas_cristin.sistema_climalert.models.Alerta;
-import ar.edu.utn.edu.lucas_cristin.sistema_climalert.models.Medicion;
-import ar.edu.utn.edu.lucas_cristin.sistema_climalert.repositories.interfaces.concretos.MedicionRepositorio;
+import ar.edu.utn.edu.lucas_cristin.sistema_climalert.repositories.interfaces.concretos.AlertaRepositorio;
 import ar.edu.utn.edu.lucas_cristin.sistema_climalert.repositories.memory.RepositorioGenericoMemoria;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AlertaRepositorioImplMemoria extends RepositorioGenericoMemoria<Alerta> implements MedicionRepositorio {
+public class AlertaRepositorioImplMemoria extends RepositorioGenericoMemoria<Alerta> implements AlertaRepositorio {
 
     @Override
     protected Long getID(Alerta alerta){
